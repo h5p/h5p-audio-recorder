4ac4b4d519b4f42599582ee39a694809bc90474f
Tom Arild Jakobsen <tajakobsen@gmail.com>
2017-04-20 10:20:11 +0200

    Add styling to status bar and buttons
     * Add status bar styling
     * Add button styling
     * Add vu meter image
    
    Relates to HFP-975

--- src/views/RecordingWrapper.vue
+++ src/views/RecordingWrapper.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="h5p-audio-recorder-view">
     <div class="icon-microphone"></div>
-    <h2>Q: {{ title }}</h2>
+    <div class="title">Q: {{ title }}</div>
     <div class="status-bar" v-bind:class="state">{{statusMessages[state]}}</div>
 
-    <button class="button button-record" v-if="state === 'ready'" v-on:click="record">Record Answer</button>
-    <button class="button button-pause" v-if="state === 'recording'" v-on:click="pause">Pause</button>
-    <button class="button button-continue" v-if="state === 'paused'" v-on:click="record">Continue</button>
-    <button class="button button-finish" v-if="state === 'recording' || state === 'paused'" v-on:click="finish">Finish</button>
+    <div class="button-row">
+      <button v-on:click="record" class="button red" v-if="state === 'ready'">Record Answer</button>
+      <button v-on:click="pause" class="button inverse red" v-if="state === 'recording'">Pause</button>
+      <button v-on:click="record" class="button red" v-if="state === 'paused'">Continue</button>
+      <button v-on:click="finish" class="button" v-if="state === 'recording' || state === 'paused'">Finish</button>
+      <button v-on:click="download" class="button green" v-if="state === 'finished'">Download</button>
+      <button v-on:click="retry" class="button" v-if="state === 'finished'">Retry</button>
+    </div>
 
-    <button class="button button-download" v-if="state === 'finished'" v-on:click="download">Download</button>
-    <button class="button button-retry" v-if="state === 'finished'" v-on:click="retry">Retry</button>
   </div>
 </template>
 
@@ -59,7 +61,59 @@
 </script>
 
 <style>
+  .h5p-audio-recorder-view .title {
+    color: black;
+    font-size: 1.875em;
+    text-align: center;
+  }
+
+  /* status bar */
   .h5p-audio-recorder-view .status-bar {
     background-color: #f8f8f8;
+    color: #777777;
+    font-size: 1.250em;
+    line-height: 3.750em;
+    text-align: center;
+    padding: 0 1em;
+  }
+
+  .h5p-audio-recorder-view .status-bar.recording {
+    background-color: #f9e5e6;
+    color: #da5254;
+  }
+
+  .h5p-audio-recorder-view .status-bar.finished {
+    background-color: #e0f9e3;
+    color:  #20603d;
+  }
+
+  .button-row {
+    text-align: center;
+  }
+
+  .button {
+    font-size: 1.563em;
+    padding: 0.708em 1.250em;
+    border-radius: 1.375em;
+    margin: 0 0.5em;
+    border: 0;
+    display: inline-block;
+    cursor: pointer;
+    background-color: #5e5e5e;
+    color: white;
+  }
+
+  .button.red {
+    background-color: #d95354;
+  }
+
+  .button.red.inverse {
+    background-color: white;
+    border: 2px solid #d95354;
+    color: #d95354;
+  }
+
+  .button.green {
+    background-color: #1f824c;
   }
 </style>
