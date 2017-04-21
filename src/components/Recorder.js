fc8473624bc10d86c3fbb6b3589c2de133a1557c
Paal Joergensen <pal.jorgensen@gmail.com>
2017-04-21 11:14:16 +0200

    Added comments for Recorder

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -10,8 +10,39 @@ const errorMessageToCodeMap = {
   NotAllowedError: 'permission-denied'
 };
 
+/**
+ * Event triggered when recorder goes from recording to inactive
+ *
+ * @event Recorder#inactive
+ * @type {Object}
+ */
+
+/**
+ * Event triggered when recording is started
+ *
+ * @event Recorder#recording
+ * @type {Object}
+ */
+
+/**
+ * Event triggered when microphone access is denied
+ *
+ * @event Recorder#blocked
+ * @type {Object}
+ */
+
+/**
+ * The recorder class
+ *
+ * @fires Recorder#inactive
+ * @fires Recorder#recording
+ * @fires Recorder#blocked
+ */
 export default class Recorder extends H5P.EventDispatcher{
-  // Record logic API goes here
+
+  /**
+   * @constructor
+   */
   constructor() {
     super();
 
@@ -22,7 +53,7 @@ export default class Recorder extends H5P.EventDispatcher{
 
     this.state = RecorderState.inactive;
 
-    // Create a worker. This is normaløly done using a URL to the js-file
+    // Create a worker. This is normally done using a URL to the js-file
     const workerBlob = new Blob(
       [RecorderWorker.toString().replace(/^function .+\{?|\}$/g, '')],
       {type:'text/javascript'}
@@ -36,6 +67,11 @@ export default class Recorder extends H5P.EventDispatcher{
     };
   }
 
+  /**
+   * Creates a URL to a wav blob
+   *
+   * @return {Promise}
+   */
   getWavURL() {
     this.stop();
 
@@ -53,10 +89,18 @@ export default class Recorder extends H5P.EventDispatcher{
     return promise;
   }
 
+  /**
+   * Creates a URL to an mp3 blob
+   *
+   * @return {Promise}
+   */
   getMP3Url() {
     // TODO
   }
 
+  /**
+   * Initialize microphone
+   */
   init() {
     const self = this;
     this.userMedia = new Promise((resolve, reject) => {
@@ -102,6 +146,9 @@ export default class Recorder extends H5P.EventDispatcher{
     });
   }
 
+  /**
+   * Start or resume a recording
+   */
   start() {
     if (!this.userMedia) {
       this.init();
@@ -116,14 +163,24 @@ export default class Recorder extends H5P.EventDispatcher{
       });
   }
 
+  /**
+   * Stop/pause a recording
+   */
   stop() {
     this._setState(RecorderState.inactive);
   }
 
+  /**
+   * Check if browser supports recording
+   * @return {boolean}
+   */
   supported() {
     return AudioContext && navigator.mediaDevices.getUserMedia;
   }
 
+  /**
+   * Reset previous recording
+   */
   reset() {
     this._setState(RecorderState.inactive);
     this.worker.postMessage({
@@ -131,6 +188,13 @@ export default class Recorder extends H5P.EventDispatcher{
     });
   }
 
+  /**
+   * Maps different browsers error codes to a common one
+   *
+   * @param {Object} e Error object from browser
+   * @private
+   * @return {string}
+   */
   _errorToCode(e) {
     if (e.name && errorMessageToCodeMap[e.name]) {
       return errorMessageToCodeMap[e.name];
@@ -138,6 +202,12 @@ export default class Recorder extends H5P.EventDispatcher{
     return 'unknown';
   }
 
+  /**
+   * Set state
+   *
+   * @private
+   * @param {string} state
+   */
   _setState(state) {
     this.state = state;
     this.trigger(this.state);
