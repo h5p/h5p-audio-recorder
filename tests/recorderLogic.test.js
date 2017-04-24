c60eb4e2d3f5bde8ba1bb8ae2c389be8dafaead9
Tom Arild Jakobsen <tajakobsen@gmail.com>
2017-04-24 14:30:42 +0200

    Add async support to Ava

--- tests/recorderLogic.test.js
+++ tests/recorderLogic.test.js
@@ -5,9 +5,9 @@ test('Test should pass', t => {
   t.pass();
 });
 
-/*
+
 test('Async test should pass', async t => {
   const foo = Promise.resolve('foo');
   const answer = await foo;
   t.is(answer, 'foo');
-});*/
+});
