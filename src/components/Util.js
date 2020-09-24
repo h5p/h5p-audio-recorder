923800b451341ccf226a678db69a415719f55028
Oliver Tacke <o.tacke@posteo.de>
2020-09-24 13:09:35 +0200

    Add non-optional default parameters in constructor

---
+++ src/components/Util.js
@@ -0,0 +1,25 @@
+/** Class for utility functions */
+class Util {
+  /**
+   * Extend an array just like JQuery's extend.
+   * @param {object} arguments Objects to be merged.
+   * @return {object} Merged objects.
+   */
+  static extend() {
+    for (let i = 1; i < arguments.length; i++) {
+      for (let key in arguments[i]) {
+        if (arguments[i].hasOwnProperty(key)) {
+          if (typeof arguments[0][key] === 'object' && typeof arguments[i][key] === 'object') {
+            this.extend(arguments[0][key], arguments[i][key]);
+          }
+          else {
+            arguments[0][key] = arguments[i][key];
+          }
+        }
+      }
+    }
+    return arguments[0];
+  }
+}
+
+export default Util;
